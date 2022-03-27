93e5c2edfb77db3752cd5d197b5c3958d110b5c1
easso <easso1@protonmail.com>
2022-03-26 17:05:41 -0700

    update

--- workshop5.js
+++ workshop5.js
@@ -7,45 +7,63 @@ class Student {
 
 
 class Bootcamp {
-    constructor(name, level, students = []) {
-        this.name = name;
-        this.level = level
-        this.students = students;
+  constructor(name, level, students = []) {
+    this.name = name;
+    this.level = level
+    this.students = students;
+  }
+
+  registerStudent(studentToRegister) { //adding method registerStudent to bootcamp class with a single parameter studentToRegister
+
+    if (studentToRegister == undefined) { 
+      console.log("Invalid Name or Email");
+      return false;
+    }   
+
+    if (!studentToRegister.name || !studentToRegister.email) {
+      console.log("Invalid Name or Email");
+      return false;
     }
-        logstudents(){
-            console.log(this.students);
-        }
 
-    registerStudent(studentToRegister) {
-        if (!studentToRegister.name || !studentToRegister.email) {
-            console.log("Invalid Name or Email");
-            return false;
-        }
-        for (const student of this.students){
-            console.log('student is', student.name)
-         if (studentToRegister.email == student.email){
-            console.log("Email is already registered");
-            return false;
-            }
-        }
-    }  
+    const foundEmail = this.students.find((currentStudent) => {
+      return currentStudent.email === studentToRegister.email;
+    })
+    
+    if (foundEmail == undefined) {
+      this.students.push(studentToRegister);
+      console.log(`Success ${studentToRegister.name} has registered to ${this.name}`);
+      return true;
+    }
+
+    else {
+      console.log("email is already register");
+      return false;
+    }
+  }
+  
+  listStudents() {
+    if (this.students.length == 0) { //need to check if this.students array is empty so it can't = 0 
+      console.log(`No students are registered to the ${this.name} bootcamp.`);
+      return false;
+    }
+
+    else {
+      console.log(`The students registered in ${this.name} are:`);
+    }
+    this.students.forEach((student) => {
+      console.log(`${student.name}, ${student.email}`); 
+    })
+    return true;
+  }
 }
-
-
 
 testStudent = new Student('Bugs Bunny', 'bugs@bunny.com');
 console.log(testStudent); 
-reactBootcamp = new Bootcamp("React", "Advanced", [testStudent]);
-reactBootcamp.registerStudent(testStudent);
-
-
-
-
 
 
 
 if ( testStudent.name === 'Bugs Bunny' && testStudent.email === 'bugs@bunny.com') {
-console.log('TASK 1: PASS');
+  console.log('TASK 1: PASS');
 }
 
 
@@ -57,12 +75,21 @@ console.log('TASK 2: PASS');
 }
 
 const runTest = (bootcamp, student) => {
-const attemptOne = bootcamp.registerStudent(student);
-const attemptTwo = bootcamp.registerStudent(student);
-const attemptThree = bootcamp.registerStudent(new Student("Babs Bunny"));
-if ( attemptOne && !attemptTwo && !attemptThree) {
-    console.log("TASK 3: PASS");
-}
+  const attemptOne = bootcamp.registerStudent(student);
+  const attemptTwo = bootcamp.registerStudent(student);
+  const attemptThree = bootcamp.registerStudent(new Student("Babs Bunny"));
+  if ( attemptOne && !attemptTwo && !attemptThree) {
+      console.log("TASK 3: PASS");
+  }
+  bootcamp.registerStudent(new Student('Babs Bunny', 'babs@bunny.com'));
+  if (bootcamp.listStudents()) {
+      console.log("TASK 4: PASS 1/2");
+  }
+  bootcamp.students = [];
+  if (!bootcamp.listStudents()) {
+      console.log("TASK 4: PASS 2/2");
+  }
 };
+
 
 runTest(reactBootcamp, testStudent);
